e8d0a7dd0a8da032492974b29cd579e366852216
Hexagon Digital Services <hexagonsservices@gmail.com>
2026-02-14 16:12:02 +0530

    Delete src/components/Navbar.jsx

--- src/components/Navbar.jsx
+++
@@ -1,6 +0,0 @@
-const navLinks = [
-    { name: "Home", path: "/" },
-    { name: "Features", path: "#features" },
-    { name: "About", path: "#about" },
-    { name: "Contact", path: "#contact" },
-];
